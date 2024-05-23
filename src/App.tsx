09e05c83ef210852f0d35dd74815a896b5c15fbb
abel <abel@altomobile.io>
2024-05-23 13:55:06 -0600

    hack

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,55 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 
+// Mapping of Safari versions to macOS versions
+const safariToMacOS = [
+  { safariVersion: "14.1.2", macOSVersion: "10.14" },
+  {
+    safariVersion: "15.6.1",
+    macOSVersion: "10.15",
+  },
+  {
+    safariVersion: "16.6.1",
+    macOSVersion: "11",
+  },
+  {
+    safariVersion: "17.5",
+    macOSVersion: ">=12",
+  },
+];
+
+function getMacOSVersion(safariVersion: string) {
+  console.log("safariVersion", safariVersion);
+
+  const match = safariToMacOS.find((entry) => {
+    console.log("comparing", entry.safariVersion, String(safariVersion));
+    return entry.safariVersion === String(safariVersion);
+  });
+
+  console.log("match", match);
+
+  if (match) {
+    return match.macOSVersion;
+  } else {
+    return "Unknown";
+  }
+}
+
+// Function to extract Safari version from the user agent string
+function getSafariVersion() {
+  const userAgent = navigator.userAgent;
+  const safariVersionMatch = userAgent.match(/Version\/(\d+\.\d+)/);
+
+  if (safariVersionMatch) {
+    return safariVersionMatch[1];
+  } else {
+    return null;
+  }
+}
+
 function App() {
   const [os, setOs] = useState("loading...");
+  const [safariVersion, setSafariVersion] = useState("loading...");
 
   useEffect(() => {
     async function fetchOs() {
@@ -25,10 +72,21 @@ function App() {
       }
     }
 
+    const safariVersion = getSafariVersion();
+
+    if (safariVersion) {
+      setSafariVersion(getMacOSVersion(safariVersion));
+    }
+
     fetchOs();
   }, []);
 
-  return <div>{os}</div>;
+  return (
+    <div>
+      <div>Experimental Api says: {os}</div>
+      <div>Safari version to OS says: {safariVersion}</div>
+    </div>
+  );
 }
 
 export default App;
